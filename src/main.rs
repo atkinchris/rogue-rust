71f9aaeee4dcd1237fce96162e8feed7c5bb84f5
Chris Atkin <atkinchris@gmail.com>
2018-10-26 14:17:46 +0100

    Add Amethyst hello world

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,35 @@
-#[macro_use]
-extern crate specs_derive;
-extern crate specs;
+extern crate amethyst;
 
-use specs::{Builder, DispatcherBuilder, ReadStorage, System, VecStorage, World, WriteStorage};
+use amethyst::{
+    prelude::*,
+    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    utils::application_root_dir,
+};
 
-struct HelloWorld;
+struct Example;
 
-impl<'a> System<'a> for HelloWorld {
-    type SystemData = ReadStorage<'a, Position>;
+impl<'a, 'b> SimpleState<'a, 'b> for Example {}
 
-    fn run(&mut self, position: Self::SystemData) {
-        use specs::Join;
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
 
-        for position in position.join() {
-            println!("Hello, {:?}", &position);
-        }
-    }
-}
+    let path = format!(
+        "{}/resources/display_config.ron",
+        application_root_dir()
+    );
+    let config = DisplayConfig::load(&path);
 
-struct UpdatePos;
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosNormTex>::new()),
+    );
 
-impl<'a> System<'a> for UpdatePos {
-    type SystemData = (ReadStorage<'a, Movement>, WriteStorage<'a, Position>);
+    let game_data =
+        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let mut game = Application::new("./", Example, game_data)?;
 
-    fn run(&mut self, (mov, mut pos): Self::SystemData) {
-        use specs::Join;
-        for (mov, pos) in (&mov, &mut pos).join() {
-            pos.x += mov.x;
-            pos.y += mov.y;
-        }
-    }
-}
+    game.run();
 
-#[derive(Component, Debug)]
-#[storage(VecStorage)]
-struct Position {
-    x: u32,
-    y: u32,
-}
-
-#[derive(Component, Debug)]
-#[storage(VecStorage)]
-struct Movement {
-    x: u32,
-    y: u32,
-}
-
-fn main() {
-    let mut world = World::new();
-
-    let mut dispatcher = DispatcherBuilder::new()
-        .with(HelloWorld, "hello_world", &[])
-        .with(UpdatePos, "update_pos", &["hello_world"])
-        .with(HelloWorld, "hello_updated", &["update_pos"])
-        .build();
-
-    dispatcher.setup(&mut world.res);
-    world.create_entity().with(Position { x: 4, y: 7 }).build();
-    world
-        .create_entity()
-        .with(Position { x: 2, y: 5 })
-        .with(Movement { x: 1, y: 0 })
-        .build();
-
-    dispatcher.dispatch(&mut world.res);
+    Ok(())
 }
