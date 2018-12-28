151ac43012dbcf62ea73ef53d786e4e845222669
Chris Atkin <atkinchris@gmail.com>
2018-12-28 16:57:49 +0000

    Add more bundles to pipeline and builder

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate amethyst;
 
 use amethyst::{
+    core::TransformBundle,
+    input::InputBundle,
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::{ColorMask, DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage, ALPHA},
     utils::application_root_dir,
 };
 
@@ -13,21 +15,30 @@ impl SimpleState for Example {}
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
-    let path = format!(
-        "{}/resources/display_config.ron",
-        application_root_dir()
-    );
-    let config = DisplayConfig::load(&path);
+    let resources_path = format!("{}/resources", application_root_dir());
+    let display_config_path = format!("{}/display.ron", resources_path);
+    let key_bindings_path = format!("{}/input.ron", resources_path);
+
+    let display_config = DisplayConfig::load(&display_config_path);
 
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
             .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
+            .with_pass(DrawFlat2D::new().with_transparency(ColorMask::all(), ALPHA, None)),
     );
 
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
-    let mut game = Application::new("./", Example, game_data)?;
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            InputBundle::<String, String>::new().with_bindings_from_file(&key_bindings_path)?,
+        )?
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            RenderBundle::new(pipe, Some(display_config))
+                .with_sprite_sheet_processor()
+                .with_sprite_visibility_sorting(&[]),
+        )?;
+
+    let mut game = Application::new(resources_path, Example, game_data)?;
 
     game.run();
 
